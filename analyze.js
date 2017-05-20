7d1a8a34df8c19fd21dbff4defb8ad78a565148c
mj-e <matt_evans30@yahoo.com>
2017-05-20 20:48:36 +0100

    added papaparse

--- analyze.js
+++ analyze.js
@@ -1,9 +1,50 @@
-var fs = require("fs");
-var d3 = require("d3");
-var _ = require("lodash");
+function parseData(createGraph) {
+	Papa.parse("./USDJPY,D1.csv", {
+		download: true,
+		complete: function (results) {
+			createGraph(results.data);
+		}
+	});
+}
 
-fs.readFile("./USDJPY,D1.csv", "utf8", function(error, data) {
-  data = d3.tsvParse(data);
-  console.log(JSON.stringify(data));
+function createGraph(data) {
+	var time = [];
+	var price = ["Yen"];
 
-});
+	for (var i = 1; i < data.length; i++) {
+		time.push(data[i][0]);
+		price.push(data[i][5]);
+	}
+
+	console.log(time);
+	console.log(price);
+
+	var chart = c3.generate({
+		bindto: '#chart',
+		data: {
+			columns: [
+				price
+			]
+		},
+		axis: {
+			x: {
+				type: 'category',
+				categories: time,
+				tick: {
+					multiline: false,
+					culling: {
+						max: 15
+					}
+				}
+			}
+		},
+		zoom: {
+			enabled: true
+		},
+		legend: {
+			position: 'right'
+		}
+	});
+}
+
+parseData(createGraph);

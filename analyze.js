0920c0db7dd570b09a0fd66b0f9173c4c52f0336
mj-e <matt_evans30@yahoo.com>
2017-05-20 19:31:52 +0100

    added readfile

--- analyze.js
+++ analyze.js
@@ -1,3 +1,9 @@
 var fs = require("fs");
 var d3 = require("d3");
 var _ = require("lodash");
+
+fs.readFile("./USDJPY,D1.csv", "utf8", function(error, data) {
+  data = d3.tsvParse(data);
+  console.log(JSON.stringify(data));
+
+});
